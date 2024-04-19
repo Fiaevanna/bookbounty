ba8937b925d07c638e76c321a03181c6cd35271a
Fia <fiaevanna@gmail.com>
2024-04-19 19:16:34 +0200

    added text header and style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,20 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Balthazar } from "next/font/google";
+import { Adamina } from "next/font/google";
 
+const balthazar = Balthazar({ subsets: ["latin"], weight: ["400"] });
+const adamina = Adamina({ subsets: ["latin"], weight: ["400"] });
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <style jsx global>{`
+        :root {
+          --special-font: ${balthazar.style.fontFamily}; 
+          --welcome-font: ${adamina.style.fontFamily}; 
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
