709e01525772ff46009e9bb22695e919b8a2895a
Fia <fiaevanna@gmail.com>
2024-05-11 00:51:43 +0200

    added some config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
+
+import withSerwistInit from "@serwist/next";
+
+const withSerwist = withSerwistInit({
+  // Note: This is only an example. If you use Pages Router,
+  // use something else that works, such as "service-worker/index.ts".
+  swSrc: "service-worker/index.ts",
+  swDest: "public/sw.js",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withSerwist(nextConfig);
